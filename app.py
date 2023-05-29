224ebd4926c0f98703ffa8857a0284c8e59b1f28
Prabhat Singh <85671922+prabhatsingh985@users.noreply.github.com>
2023-05-29 20:28:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask,redirect,url_for,render_template,Response
+import cv2 
+# WSGI APPLILCTION
+app = Flask(__name__)
+
+app=Flask(__name__)
+camera = cv2.VideoCapture(0) 
+
+def func():
+     while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            detector=cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
+            eye_cascade = cv2.CascadeClassifier('Haarcascades/haarcascade_eye.xml')
+            faces=detector.detectMultiScale(frame,1.1,7)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+             #Draw the rectangle around each face
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (46,4,4), 2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255,255,0), 2)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')        
+             
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video')
+def video():
+       return Response(func(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__=='__main__':
+    app.run(debug=True)
+ 
+ 
+
